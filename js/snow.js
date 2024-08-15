59e2bbf24d3226281de240f07c8d6d9b524a177d
FnafyyBoyy <168121681+FnafyyBoyy@users.noreply.github.com>
2024-08-15 02:03:37 +0200

    Website

---
+++ js/snow.js
@@ -0,0 +1,42 @@
+particlesJS("SnowFall",
+    {
+        "particles": {
+            "number": {
+                "value": 80
+            },
+            "shape": {
+                "type": "circle",
+             },
+            "size": {
+                "value": 7,
+                "random": true
+            },
+            "line_linked": {
+                "enable": false
+            },
+            "move": {
+                "enable": true,
+                "speed": 5,
+                "direction": "bottom",
+                "straight": false
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": false
+                }
+            },
+            "modes": {
+                "push": {
+                    "particles_nb": 12
+                }
+            }
+        }
+    }
+
+
+);
+
+//The Main Code Goes to: https://bybilly.uk/. Redesigned and Coded by: https://wuemeli.com
